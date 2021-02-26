9b77747d2c19c2723c0802a92fe1b87d69829d85
suragch <studymongolian@gmail.com>
2021-02-26 15:31:54 +0800

    exporting MongolTextField. Good enough for external alpha testing

--- lib/mongol.dart
+++ lib/mongol.dart
@@ -3,3 +3,6 @@ library mongol;
 export 'package:mongol/mongol_text.dart';
 export 'package:mongol/mongol_rich_text.dart';
 export 'package:mongol/mongol_alert_dialog.dart';
+export 'package:mongol/mongol_text_field.dart';
+export 'package:mongol/alignment.dart';
+export 'package:mongol/input_border.dart';
