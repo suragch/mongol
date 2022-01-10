1c3ddd0370658eec2c65baf7c86f23737f0dd143
suragch <studymongolian@gmail.com>
2022-01-10 15:07:09 +0800

    export MongolTextPainter

--- lib/mongol.dart
+++ lib/mongol.dart
@@ -3,6 +3,7 @@ library mongol;
 export 'package:mongol/src/text/mongol_text.dart';
 export 'package:mongol/src/text/mongol_rich_text.dart';
 export 'package:mongol/src/base/mongol_text_align.dart';
+export 'package:mongol/src/base/mongol_text_painter.dart';
 export 'package:mongol/src/dialog/mongol_alert_dialog.dart';
 export 'package:mongol/src/menu/mongol_popup_menu.dart';
 export 'package:mongol/src/menu/mongol_tooltip.dart';
