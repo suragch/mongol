c8c44e17874b06813bc141a6b9971b878c356097
suragch <suragch@suragch.dev>
2022-10-22 12:56:02 +0800

    input decoration demo

--- example/lib/demos/input_decorations_demo.dart
+++ example/lib/demos/input_decorations_demo.dart
@@ -25,6 +25,7 @@ class _InputDecorationsBodyState extends State<InputDecorationsBody> {
   static BorderType _borderValue = BorderType.outline;
   bool _showOutlineBorder = true;
   bool _showSidelineBorder = false;
+  bool _filled = false;
   bool _showSize = false;
   bool _showLabel = false;
   bool _showLabelText = false;
@@ -58,6 +59,8 @@ class _InputDecorationsBodyState extends State<InputDecorationsBody> {
   );
   static const text = 'ᠨᠢᠭᠡ ᠬᠤᠶᠠᠷ ᠭᠤᠷᠪᠠ';
   final icon = const Icon(Icons.star);
+  final prefixIcon = const Icon(Icons.favorite);
+  final suffixIcon = const Icon(Icons.park);
   final redBorder = BoxDecoration(
     border: Border.all(width: 1, color: Colors.red),
   );
@@ -66,136 +69,175 @@ class _InputDecorationsBodyState extends State<InputDecorationsBody> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CheckboxItem(
-              title: 'Outline border',
-              value: _showOutlineBorder,
-              onChanged: (newValue) {
-                _showOutlineBorder = newValue!;
-                if (_showOutlineBorder) {
-                  _showSidelineBorder = false;
-                  _borderValue = BorderType.outline;
-                } else {
-                  _borderValue = BorderType.none;
-                }
-                setState(() {});
-              },
-            ),
-            CheckboxItem(
-              title: 'Sideline border',
-              value: _showSidelineBorder,
-              onChanged: (newValue) {
-                _showSidelineBorder = newValue!;
-                if (_showSidelineBorder) {
-                  _showOutlineBorder = false;
-                  _borderValue = BorderType.sideline;
-                } else {
-                  _borderValue = BorderType.none;
-                }
-                setState(() {});
-              },
-            ),
-            CheckboxItem(
-              title: 'Show size',
-              value: _showSize,
-              onChanged: (newValue) {
-                setState(() => _showSize = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Label text',
-              value: _showLabelText,
-              onChanged: (newValue) {
-                setState(() => _showLabelText = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Label widget',
-              value: _showLabel,
-              onChanged: (newValue) {
-                setState(() => _showLabel = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Hint',
-              value: _showHint,
-              onChanged: (newValue) {
-                setState(() => _showHint = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Error',
-              value: _showError,
-              onChanged: (newValue) {
-                setState(() => _showError = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Counter text',
-              value: _showCounterText,
-              onChanged: (newValue) {
-                setState(() => _showCounterText = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Counter widget',
-              value: _showCounterWidget,
-              onChanged: (newValue) {
-                setState(() => _showCounterWidget = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Icon',
-              value: _showIcon,
-              onChanged: (newValue) {
-                setState(() => _showIcon = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Prefix icon',
-              value: _showPrefixIcon,
-              onChanged: (newValue) {
-                setState(() => _showPrefixIcon = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Prefix text',
-              value: _showPrefixText,
-              onChanged: (newValue) {
-                setState(() => _showPrefixText = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Prefix widget',
-              value: _showPrefix,
-              onChanged: (newValue) {
-                setState(() => _showPrefix = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Suffix icon',
-              value: _showSuffixIcon,
-              onChanged: (newValue) {
-                setState(() => _showSuffixIcon = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Suffix text',
-              value: _showSuffixText,
-              onChanged: (newValue) {
-                setState(() => _showSuffixText = newValue!);
-              },
-            ),
-            CheckboxItem(
-              title: 'Suffix widget',
-              value: _showSuffix,
-              onChanged: (newValue) {
-                setState(() => _showSuffix = newValue!);
-              },
-            ),
-          ],
+        SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CheckboxItem(
+                title: 'Outline border',
+                value: _showOutlineBorder,
+                onChanged: (newValue) {
+                  _showOutlineBorder = newValue!;
+                  if (_showOutlineBorder) {
+                    _showSidelineBorder = false;
+                    _borderValue = BorderType.outline;
+                  } else {
+                    _borderValue = BorderType.none;
+                  }
+                  setState(() {});
+                },
+              ),
+              CheckboxItem(
+                title: 'Sideline border',
+                value: _showSidelineBorder,
+                onChanged: (newValue) {
+                  _showSidelineBorder = newValue!;
+                  if (_showSidelineBorder) {
+                    _showOutlineBorder = false;
+                    _borderValue = BorderType.sideline;
+                  } else {
+                    _borderValue = BorderType.none;
+                  }
+                  setState(() {});
+                },
+              ),
+              CheckboxItem(
+                title: 'Show size',
+                value: _showSize,
+                onChanged: (newValue) {
+                  setState(() => _showSize = newValue!);
+                },
+              ),
+              CheckboxItem(
+                title: 'Filled',
+                value: _filled,
+                onChanged: (newValue) {
+                  setState(() => _filled = newValue!);
+                },
+              ),
+              CheckboxItem(
+                title: 'Label text',
+                value: _showLabelText,
+                onChanged: (newValue) {
+                  setState(() {
+                    _showLabelText = newValue!;
+                    if (_showLabelText && _showLabel) {
+                      _showLabel = false;
+                    }
+                  });
+                },
+              ),
+              CheckboxItem(
+                title: 'Label widget',
+                value: _showLabel,
+                onChanged: (newValue) {
+                  setState(() {
+                    _showLabel = newValue!;
+                    if (_showLabelText && _showLabel) {
+                      _showLabelText = false;
+                    }
+                  });
+                },
+              ),
+              CheckboxItem(
+                title: 'Hint',
+                value: _showHint,
+                onChanged: (newValue) {
+                  setState(() => _showHint = newValue!);
+                },
+              ),
+              CheckboxItem(
+                title: 'Error',
+                value: _showError,
+                onChanged: (newValue) {
+                  setState(() => _showError = newValue!);
+                },
+              ),
+              CheckboxItem(
+                title: 'Counter text',
+                value: _showCounterText,
+                onChanged: (newValue) {
+                  setState(() => _showCounterText = newValue!);
+                },
+              ),
+              CheckboxItem(
+                title: 'Counter widget',
+                value: _showCounterWidget,
+                onChanged: (newValue) {
+                  setState(() => _showCounterWidget = newValue!);
+                },
+              ),
+              CheckboxItem(
+                title: 'Icon',
+                value: _showIcon,
+                onChanged: (newValue) {
+                  setState(() => _showIcon = newValue!);
+                },
+              ),
+              CheckboxItem(
+                title: 'Prefix icon',
+                value: _showPrefixIcon,
+                onChanged: (newValue) {
+                  setState(() => _showPrefixIcon = newValue!);
+                },
+              ),
+              CheckboxItem(
+                title: 'Prefix text',
+                value: _showPrefixText,
+                onChanged: (newValue) {
+                  setState(() {
+                    _showPrefixText = newValue!;
+                    if (_showPrefixText && _showPrefix) {
+                      _showPrefix = false;
+                    }
+                  });
+                },
+              ),
+              CheckboxItem(
+                title: 'Prefix widget',
+                value: _showPrefix,
+                onChanged: (newValue) {
+                  setState(() {
+                    _showPrefix = newValue!;
+                    if (_showPrefixText && _showPrefix) {
+                      _showPrefixText = false;
+                    }
+                  });
+                },
+              ),
+              CheckboxItem(
+                title: 'Suffix icon',
+                value: _showSuffixIcon,
+                onChanged: (newValue) {
+                  setState(() => _showSuffixIcon = newValue!);
+                },
+              ),
+              CheckboxItem(
+                title: 'Suffix text',
+                value: _showSuffixText,
+                onChanged: (newValue) {
+                  setState(() {
+                    _showSuffixText = newValue!;
+                    if (_showSuffixText && _showSuffix) {
+                      _showSuffix = false;
+                    }
+                  });
+                },
+              ),
+              CheckboxItem(
+                title: 'Suffix widget',
+                value: _showSuffix,
+                onChanged: (newValue) {
+                  setState(() {
+                    _showSuffix = newValue!;
+                    if (_showSuffixText && _showSuffix) {
+                      _showSuffixText = false;
+                    }
+                  });
+                },
+              ),
+            ],
+          ),
         ),
         Expanded(
           child: Column(
@@ -209,6 +251,8 @@ class _InputDecorationsBodyState extends State<InputDecorationsBody> {
                       border: (_borderValue == BorderType.sideline)
                           ? const UnderlineInputBorder()
                           : _borderValue.border,
+                      filled: _filled,
+                      fillColor: Colors.blue.shade100,
                       labelText: (_showLabelText) ? text : null,
                       label: (_showLabel) ? box : null,
                       hintText: (_showHint) ? text : null,
@@ -217,10 +261,10 @@ class _InputDecorationsBodyState extends State<InputDecorationsBody> {
                       counter: (_showCounterWidget) ? box : null,
                       icon: (_showIcon) ? icon : null,
                       prefix: (_showPrefix) ? box : null,
-                      prefixIcon: (_showPrefixIcon) ? icon : null,
+                      prefixIcon: (_showPrefixIcon) ? prefixIcon : null,
                       prefixText: (_showPrefixText) ? text : null,
                       suffix: (_showSuffix) ? box : null,
-                      suffixIcon: (_showSuffixIcon) ? icon : null,
+                      suffixIcon: (_showSuffixIcon) ? suffixIcon : null,
                       suffixText: (_showSuffixText) ? text : null,
                     ),
                   ),
@@ -234,6 +278,8 @@ class _InputDecorationsBodyState extends State<InputDecorationsBody> {
                     child: MongolTextField(
                       decoration: InputDecoration(
                         border: _borderValue.border,
+                        filled: _filled,
+                        fillColor: Colors.blue.shade100,
                         labelText: (_showLabelText) ? text : null,
                         label: (_showLabel) ? box : null,
                         hintText: (_showHint) ? text : null,
@@ -242,10 +288,10 @@ class _InputDecorationsBodyState extends State<InputDecorationsBody> {
                         counter: (_showCounterWidget) ? box : null,
                         icon: (_showIcon) ? icon : null,
                         prefix: (_showPrefix) ? box : null,
-                        prefixIcon: (_showPrefixIcon) ? icon : null,
+                        prefixIcon: (_showPrefixIcon) ? prefixIcon : null,
                         prefixText: (_showPrefixText) ? text : null,
                         suffix: (_showSuffix) ? box : null,
-                        suffixIcon: (_showSuffixIcon) ? icon : null,
+                        suffixIcon: (_showSuffixIcon) ? suffixIcon : null,
                         suffixText: (_showSuffixText) ? text : null,
                       ),
                     ),
