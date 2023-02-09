ac0312a91080cd716bd83ec84347509966eda50f
suragch <suragch@suragch.dev>
2023-02-09 19:03:55 +0800

    exporting MongolEditableText for testing

---
+++ example/lib/demos/editable_text_demo.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:mongol/mongol.dart';
+
+class MongolEditableTextDemo extends StatelessWidget {
+  const MongolEditableTextDemo({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('MongolEditableText')),
+      body: const SearchBody(),
+    );
+  }
+}
+
+class SearchBody extends StatefulWidget {
+  const SearchBody({Key? key}) : super(key: key);
+  @override
+  State<SearchBody> createState() => _SearchBodyState();
+}
+
+class _SearchBodyState extends State<SearchBody> {
+  TextEditingController controller = TextEditingController();
+  final focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+          child: MongolEditableText(
+            style: const TextStyle(fontSize: 24, color: Colors.black),
+            controller: controller,
+            maxLines: null,
+            cursorColor: Colors.blue,
+            focusNode: focusNode,
+            autofocus: true,
+          ),
+        ),
+      ),
+    );
+  }
+}
