b4479e87682504d82bc15c876f254beda2e9c266
Satsrag <satsrag@outlook.com>
2023-02-10 22:39:26 +0800

    fix: import relative path

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mongol/mongol.dart';
-import 'package:mongol_demo_app/demos/text_input_control_keyboard_demo.dart';
 
 import 'demos/alert_dialog_demo.dart';
 import 'demos/button_demo.dart';
@@ -10,6 +9,7 @@ import 'demos/horizontal_listview_demo.dart';
 import 'demos/input_decorations_demo.dart';
 import 'demos/input_shortcuts_demo.dart';
 import 'demos/keyboard_demo.dart';
+import 'demos/text_input_control_keyboard_demo.dart';
 import 'demos/list_tile_demo.dart';
 import 'demos/max_lines_demo.dart';
 import 'demos/mongol_text_field_demo.dart';
