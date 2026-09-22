52dd6effbbcd58682621a7c998d2611b9bdede28
suragch <suragch@suragch.dev>
2026-09-22 11:01:04 -0700

    recreate platforms

---
+++ example/android/app/src/main/kotlin/com/example/mongol_demo_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mongol_demo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- example/android/app/src/main/kotlin/net/studymongolian/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package net.studymongolian.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
